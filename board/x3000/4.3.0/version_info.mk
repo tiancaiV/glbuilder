e64be12071140400504cbea878a492c7d21c596c
Hongjian Zhang <hongjian.zhang@gl-inet.com>
2023-05-12 11:51:12 +0800

    add x3000

---
+++ board/x3000/4.3.0/version_info.mk
@@ -0,0 +1,4 @@
+MODEL_PROFILE:=glinet_gl-x3000
+imagebuilder_hash:=39e69b28d58cda1b0f7b24183d70c99e95b023dcfbafdea3878c9c9df2286127
+sdk_hash:=1c7da50f187ed91fe59a119da753b51ae2c1c0d1f888635c1c36b2d25b29daff
+gl_collision_package:= -dnsmasq
