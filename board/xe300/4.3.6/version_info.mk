908be112e070ffcd7f0291af6dcbc01988dfccf9
Luo Chongjun <luochongjun@gl-inet.com>
2023-08-10 17:59:25 +0800

    add xe300 support

---
+++ board/xe300/4.3.6/version_info.mk
@@ -0,0 +1,4 @@
+MODEL_PROFILE:=glinet_gl-xe300-nor-nand
+imagebuilder_hash:=9154350bdc26a14a2b9a5bc3f40222ac493a04c0ca397f16261dacba13bfa1a5
+sdk_hash:=681a30b4085945c21730bace03a1eff96ad90ba33b911919070b5c6ec234edc5
+gl_collision_package:= -dnsmasq -wpad-basic-wolfssl -libustream-wolfssl
